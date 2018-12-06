c9a63509adbc4d550f3002a52b8a84116ade662b
Vos, M.A. (Martijn) <martijn.vos1@devolksbank.nl>
2018-12-06 21:09:24 +0100

    Added default image

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./Gallery.css";
 
+import HotDog from "../../assets/hot-dog.jpg";
+
 // TODO: implement logic to load in image to categorize
-const Gallery = () => <img alt="item to categorize" />;
+const Gallery = () => (
+  <img className="image" src={HotDog} alt="item to categorize" />
+);
 
 export default Gallery;
