da4f9fc5cb46e309e86b404906aab11157070114
Vos, M.A. (Martijn) <martijn.vos1@devolksbank.nl>
2018-12-06 22:54:00 +0100

    Switched button placement and gave them a distinctive color

--- src/components/CategorizationButtons/index.js
+++ src/components/CategorizationButtons/index.js
@@ -4,11 +4,11 @@ import "./CategorizationButtons.css";
 const CategorizationButtons = () => (
   <div className="button-container">
     {/* TODO: make the buttons categorize the shown image when clicked */}
-    <button type="button" className="button">
-      Hotdog
-    </button>
-    <button type="button" className="button">
+    <button type="button" className="button button-red">
       Not hotdog
+    </button>
+    <button type="button" className="button button-green">
+      Hotdog
     </button>
   </div>
 );
