ea96cab5abef3d9ccda44190cd9e78607c7d39eb
Vos, M.A. (Martijn) <martijn.vos1@devolksbank.nl>
2018-12-06 21:39:24 +0100

    Made absence of categorized image number more clear

--- src/components/InformativeText/index.js
+++ src/components/InformativeText/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./InformativeText.css";
 
 const InformativeText = ({ amountCategorized }) => (
-  <p>You have already categorized {amountCategorized} images</p>
+  <p>You have already categorized ... {amountCategorized} images</p>
 );
 
 export default InformativeText;
